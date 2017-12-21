2508d5421ee9673bbffad1a2b31723807f1e010e
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-12-21 11:12:33 +0100

    Cosas

---
+++ Practica/Assets/Scripts/BlobTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobTrigger : MonoBehaviour {
+
+    private SpriteRenderer spriteJugador;
+    private SpriteRenderer spriteBlob;
+    private GameObject jugador;
+	// Use this for initialization
+	void Start () {
+        jugador = GameObject.Find("Character");
+        spriteJugador = jugador.GetComponent<SpriteRenderer>();
+        spriteBlob = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        spriteJugador.sortingOrder = 3;
+        spriteBlob.sortingOrder = 2;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteJugador.sortingOrder = 2;
+        spriteBlob.sortingOrder = 3;
+    }
+}
