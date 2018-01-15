b1b887cd4ca868f417e1e3a642ad007ba076fcb4
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2018-01-15 09:17:24 +0100

    Tengo que arreglar la camara
    No consigo que el trigger de las habitaciones reconozca la camara cuando entra.

---
+++ Practica/Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    Vector3 lastPosition;
+    Transform player;
+    public bool habitacion2;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Character").GetComponent<Transform> ();
+        lastPosition = player.position;
+        habitacion2 = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Recoger la ultima posicion en la que ha estado el jugador
+        Vector3 delta = player.position - lastPosition;
+        //Sumarle esta diferencia a la camara
+        /*this.transform.position += delta;*/
+        if (habitacion2 && lastPosition.y < 27.39 && lastPosition.y > 17.83)
+        {
+            Debug.Log(this.transform.position.y);
+            this.transform.position += new Vector3(0, delta.y, 0);
+        }
+        //Guarda posicion para el siguiente frame
+        lastPosition = player.position;
+    }
+}
---
+++ Practica/Assets/Scripts/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       // Debug.Log(this.name);
+        Debug.Log(collision.tag); //Arreglar el trigger de la camara, que al parecer no lo encuentra.
+        if (this.name == "Habitacion_Simple_2" && collision.tag == "MainCamera")
+        {
+            camera.habitacion2 = true;
+        }
+    }
+}
