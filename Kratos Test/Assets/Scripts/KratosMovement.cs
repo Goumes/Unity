fde70313cc8fb181e8a5dc7a7d31878a94011ab6
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2020-01-06 12:39:14 +0100

    God of War pixel minigame
    First git update (bit late lol)

---
+++ Kratos Test/Assets/Scripts/KratosMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KratosMovement : MonoBehaviour
+{
+
+    public KratosController controller;
+    private float horizontalInput = 0f;
+    public float runSpeed = 20f;
+    private bool wantJump = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            wantJump = true;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalInput * Time.fixedDeltaTime, wantJump);
+        wantJump = false;
+    }
+}
